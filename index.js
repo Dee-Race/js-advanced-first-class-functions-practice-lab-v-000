a4e6a571932ec60e9e65fcfcd2650a9ddd8dc3d8
Dee Race <deerace12@gmail.com>
2018-08-06 18:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,7 @@ const totalRevenue = function(drivers) {
     return currentDriver.revenue + total;
   }, 0);
 }
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
